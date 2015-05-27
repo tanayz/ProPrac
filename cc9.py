d904e449f286ef0242624e1bf4e1deaf348f8df7
tanay006 <tanayz@outlook.com>
2015-05-27 06:16:39 -0500

    Fixed the a and b length

--- cc9.py
+++ cc9.py
@@ -1,27 +1,38 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 12 01:46:17 2015
-
+Given two sorted arrays, A and B, where A has a large enough 
+buffer at the end to hold B. Write a method to merge B into A in sorted order
 @author: tanay
 """
 
-a=[1,2,3,4,5,6,None, None, None,None, None, None]
-b=[8,9,10,11,12,13]
-m=6
-n=len(b)
-def abmerge(a,b,m,n):
-    k=m+n-1   # Index of last location of array b
-    i=n-1   # Index of last element in array b
-    j=m-1   # Index of last element in array a
-    while (i>0 & j>0):
+a=[1,2,3,4,8,9,14,15,None, None, None,None, None, None,None, None, None]
+b=[5,6,10,11,12,13,16,17,18]
+
+def abmerge(a,b):
+    k=len(a)-1   # Index of last location of array a
+    j=len(b)-1     # Index of last element in array b
+    i=len(a)-len(b)-1     # Index of last element in array a
+    while (i>=0 and j>=0):
         if(a[i]>b[j]):
-            a[--k]=a[--i]
+            a[k]=a[i]
+#            print 'i:',i,'k:',k
+            k=k-1
+            i=i-1
             
         else:
-            a[--k]=b[--j]
+            a[k]=b[j]
+#            print 'j:',j,'k:',k
+            k=k-1
+            j=j-1            
         
-    while (j>0):
-        a[--k]=b[--j]
+    while (j>=0):
+        a[k]=b[j]
+#        print 'j:',j,'k:',k
+        k=k-1
+        j=j-1
+        
     return a
     
-k=abmerge(a,b,m,n) 
+k=abmerge(a,b) 
+print k
