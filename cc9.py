64de77f89746c82435e36fb0d3cd242feeaf4b6b
tanay006 <tanayz@outlook.com>
2015-05-27 06:31:08 -0500

    Fixed the none allocation

--- cc9.py
+++ cc9.py
@@ -6,7 +6,7 @@ buffer at the end to hold B. Write a method to merge B into A in sorted order
 @author: tanay
 """
 
-a=[1,2,3,4,8,9,14,15,None, None, None,None, None, None,None, None, None]
+a=[1,2,3,4,7,8,9,14,15]+[None]*9
 b=[5,6,10,11,12,13,16,17,18]
 
 def abmerge(a,b):
